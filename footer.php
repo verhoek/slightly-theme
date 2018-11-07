be77108de5a0169726dd0e6d18b46b25082d957d
verhoek <30193551+verhoek@users.noreply.github.com>
2018-11-07 19:42:13 +0100

    Removed blog name from footer.

--- footer.php
+++ footer.php
@@ -19,7 +19,7 @@
     <div class="col-xs-12">
 
 		<div style="font-size:0.6em" class="site-info">
-            &copy; <?php echo intval( date_i18n(__('Y','slightly')) ) ?> <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a> &bull; <?php
+            &copy; <?php echo intval( date_i18n(__('Y','slightly')) ) ?> <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"></a> &bull; <?php
                 /* translators: 1: Theme name, 2: Theme author. */
                 printf( esc_html__( '%1$s.', 'slightly' ), '<a href="https://nickciliak.com/?ref=slightlytheme" rel="designer">Slightly Theme</a>' ); ?>
 		</div><!-- .site-info -->
